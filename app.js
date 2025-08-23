fda1dc537a16246d8dc4e35cf922aa73638556fc
21_guntank <soerrea@gmail.com>
2025-08-23 21:51:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
 //e.preventDefault();
 //}
 //});
+
 const THUMB_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyXwDQzx31tpoE6gw55aNoAw57o6j9H6RYEY8EaA2HCY34cq74uI_8KrFv36W9wpHo/exec?action=thumb';
 async function loadThumbToImage(idOrUrl, imgEl, placeholderUrl) {
     try {
